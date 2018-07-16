9ac602c23d2f4fd8d5d74f4af3564275c8d20360
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-07-16 12:28:01 -0400

    edited post route

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/", (req, res)=>{
 //////////////upload photo and descriptions///////////////////////
 app.post("/api", (req, res)=>{
 
-        var github = typeof req.body.githubLink;
+        var github = req.body.githubLink;
         var data= req.body.image;
 
         var contentType="image/jpg";
