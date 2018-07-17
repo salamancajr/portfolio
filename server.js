326422a1393a2a5b1a9c0e2bbcfd180f48830bbe
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-07-17 00:20:21 -0400

    changed multer

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     },
 
     filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname)
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname)
     }
 })
 var upload = multer({
