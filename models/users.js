4e493c6ddba5a6a38cbae95cd41a7e49f225e5f8
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-07-24 11:53:03 -0400

    added auth

---
+++ models/users.js
@@ -0,0 +1,110 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const bcrypt = require("bcryptjs");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    email:{
+        type:"string",
+        required:true
+    },
+    password:{
+        type:"string",
+        required:true
+    },
+    tokens:[{
+        access:{
+            type:String,
+            required:true
+        },
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+})
+
+UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ["_id", "email"])
+};
+
+UserSchema.methods.generateAuthToken = function (){
+    var user = this;
+    var access = "auth";
+    var token = jwt.sign({_id:user._id.toHexString(), access}, process.env.JWT_SECRET)
+
+    user.tokens = user.tokens.concat({access, token});
+
+    return user.save().then(()=>{
+        return token;
+    })
+}
+
+UserSchema.methods.removeToken = function(token){
+    var user = this;
+    return user.update({$pull:{
+        tokens:{
+            token
+        }
+    }})
+}
+
+UserSchema.statics.findByToken = function (token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+
+    }catch(e){
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        "_id":decoded._id,
+        "tokens.token":token,
+        "tokens.access":"auth"
+    })
+}
+
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject()
+        }
+        return new Promise((resolve, reject)=>{
+            bcrypt.compare(password, user.password, (err, ans)=>{
+                if(ans){
+                    resolve(user)
+                }
+                else{
+                    reject()
+                }
+            })
+        })
+    })
+}
+
+UserSchema.pre("save", function(next){
+    var user = this;
+    if(user.isModified("password")){
+        bcrypt.genSalt(10, (err, salt)=>{
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                user.password = hash;
+                next();
+            })
+        })
+    }
+    else{
+        next();
+    }
+})
+
+var User = mongoose.model("User", UserSchema)
+
+module.exports = {User}
