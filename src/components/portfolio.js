d767c7740f4787beac82aea11d0a5bb04496f5ac
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2019-01-07 21:45:38 -0500

    style changes to portfolio route

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -17,14 +17,14 @@ class Portfolio extends Component {
     }
 
     componentDidMount(){
-        window.addEventListener("resize", ()=>{
-            var a = document.getElementsByClassName("projects-container__project");
+        // window.addEventListener("resize", ()=>{
+        //     var a = document.getElementsByClassName("projects-container__project");
 
-            for(let i=0;i<a.length;i++){
-                a[i].style.top = 0+"px";
-            }
-            this.setState({shift:0})
-        })
+        //     for(let i=0;i<a.length;i++){
+        //         a[i].style.top = 0+"px";
+        //     }
+        //     this.setState({shift:0})
+        // })
 
         this.props.fetchProjects(()=>this.setState({isLoaded:true}));
 
