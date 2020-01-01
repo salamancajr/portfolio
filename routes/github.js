cabdc4b58a299ab4d266c144c5e4d5d83beb90b1
George Salamanca Jr <georgesalamancajr@Georges-MacBook-Pro.local>
2020-01-01 01:32:56 -0500

    webhook test

--- routes/github.js
+++ routes/github.js
@@ -1,6 +1,11 @@
+const git = require('simple-git')
+
 module.exports = app => {
   app.post('/api/updateGit', (req, res) => {
+    git().pull('origin', 'master')
     console.log('Git webhook working')
+
+    res.sendStatus(200)
   })
 }
 // Test
