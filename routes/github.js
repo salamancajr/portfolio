4b8622294af372451dae50e4b4d70cfe319ed3e8
George Salamanca Jr <georgesalamancajr@Georges-MacBook-Pro.local>
2020-01-01 04:16:27 -0500

    log headers

--- routes/github.js
+++ routes/github.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process')
 
 module.exports = app => {
   app.post('/api/updateGit', (req, res) => {
-    console.log(req.headers['X-Hub-Signature'])
+    console.log(req.headers)
     git().pull('origin', 'master').then(() => {
       execSync('pm2 reload 0')
       res.sendStatus(200)
