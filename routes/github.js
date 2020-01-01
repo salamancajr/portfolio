9c50ecc36137258124a68f5bb1f08d042c88f70a
George Salamanca Jr <georgesalamancajr@Georges-MacBook-Pro.local>
2020-01-01 03:46:38 -0500

    added execSync

--- routes/github.js
+++ routes/github.js
@@ -1,8 +1,12 @@
-const git = require('simple-git')
+// const git = require('simple-git')
+const { execSync } = require('child_process')
 
 module.exports = app => {
   app.post('/api/updateGit', (req, res) => {
-    git().pull('origin', 'master')
+    execSync('pm2 stop 0')
+    execSync('git pull origin master')
+    execSync('pm2 start 0')
+    // git().pull('origin', 'master')
 
     res.sendStatus(200)
   })
